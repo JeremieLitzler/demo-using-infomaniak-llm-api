e1e966dfc56c9aa4773ddd4d1f96ff46c2b619d4
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-05-02 13:58:45 +0200

    add suspense

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,15 @@ const fillResult = (data: LlmResult) => (result.value = data);
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <TheInput @@capture="fillResult" />
-  <TheOutput :result />
+  <suspense>
+    <template #default>
+      <TheInput @@capture="fillResult" />
+      <TheOutput :result />
+    </template>
+    <template #fallback>
+      <section class="loading">Loading...</section>
+    </template>
+  </suspense>
 </template>
 
 <style scoped>
