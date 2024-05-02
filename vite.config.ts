90d7ccad3ca7109234e51611a52ab5a6a7c4c9ae
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-05-02 15:02:37 +0200

    add proxy to solve cors issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.infomaniak.com/2/llm/701/chat/completions',
+        changeOrigin: true, // Optional: Change origin header to match frontend
+        secure: false, // Optional: Disable checks for https on target
+      },
+    },
+  },
+});
